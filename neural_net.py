e0ef1d55018c113724552f268b8a951fa7dfc910
Panos <ppetsag@gmail.com>
2020-08-16 16:29:11 +0100

    sperate neural net to a different file

---
+++ neural_net.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+
+class Model(nn.Module):
+    def __init__(self, state_size, action_size, action_range= None):
+        super().__init__()
+        if action_range==None:
+            # self.action_low, self.action_high =\
+            #     torch.from_numpy(np.array([0.]*action_size)), torch.from_numpy(np.array([1.]*action_size))#, torch.from_numpy(np.array([1.]*action_size))
+            self.range_available = False
+        else:
+            self.range_available = True
+            self.action_low, self.action_high = torch.from_numpy(np.array(action_range))
+        self.layer1 = nn.Linear(state_size, 20)
+        self.layer2 = nn.Linear(20, 20)
+        self.layer3 = nn.Linear(20, 20)
+        self.action = nn.Linear(20, action_size)
+
+    def forward(self, state):
+        m      = torch.nn.LeakyReLU(0.1)#0.01)
+        layer1 = m(self.layer1(state))
+        layer2 =m(self.layer2(layer1))
+        layer3 = m(self.layer3(layer2))
+        action = (self.action(layer3))
+        if self.range_available:
+            return self.action_low + (self.action_high - self.action_low) * (action)
+        else:
+            return (action)
--- train_example.py
+++ train_example.py
@@ -1,6 +1,6 @@
 import numpy as np
 from train_ann import *
-
+from neural_net import *
 x_m = 1000  # number of samples
 x = np.random.default_rng().uniform(-5, 5, x_m)
 y = np.random.default_rng().uniform(-5, 5, x_m)
@@ -15,7 +15,7 @@ if f.ndim==1:
     F = F.reshape(-1, 1)
 
 model = Model(X.shape[1],F.shape[1])
-ANN = train_ann(model, X, F, learning_rate=1e-3, plot=False)
+ANN = train_ann(model, X, F)
 
 
 ys = ANN.predict(X)
