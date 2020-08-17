f8aee2cc83a8e86b475c2b9a19092451501ce222
Panos <ppetsag@gmail.com>
2020-08-17 15:20:39 +0100

    include setup

---
+++ set_up.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="simpleTorch", # Replace with your own username
+    version="0.0.1",
+    author="Panos Petsagkourakis",
+    author_email="p.petsagkourakis@ucl.ac.uk",
+    description="This package is built on pytorch to avoid some standard steps",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/panos108/Training_ANN_with_Pytorch/tree/master",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
